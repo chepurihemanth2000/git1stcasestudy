f2f95755582cba6154374cabac28e7d165f665ff
root <root@ip-172-31-40-199.ap-south-1.compute.internal>
2022-08-16 00:20:45 +0000

    Edited main.c in master branch

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main{
 printf("This code is written in master branch");
 printf("Feature 1 is added");
 printf("This passed all the test cases");
+printf("This was edited in final master branch");
 return 0;
 
 }

541a0b0b9fd5793ebced458482c2ec47105922ea
Hemanth Chepuri <66219725+chepurihemanth2000@users.noreply.github.com>
2022-08-16 05:57:21 +0530

    Added some extra line from github as a trail

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ printf("This code is written in master branch");
 
 
 
+printf("Edited this in github");
 printf(" i dunno why the fuck merge conflict is occuring");
 printf("Feature 2 was added");
 return 0;
